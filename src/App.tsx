209d997355d9fb7e5f93be84e0b6d05c430b3b08
MOHIT VERMA <66687560+vermaji123089@users.noreply.github.com>
2024-01-03 10:49:04 +0530

    PORT

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,30 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
-
+import { IonApp, setupIonicReact } from '@ionic/react';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+import {About , Contact , Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+
+<BrowserRouter>
+<div className='reletive z-0 bg-primary' >
+  <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center' >
+    <Navbar/>
+    <Hero />
+  </div>
+  <About/> 
+  <Experience/>
+  <Tech/>
+  <Works/> 
+  <Feedbacks/>
+  <div className='reletive z-0'>
+    <Contact/>
+    <StarsCanvas/>
+  </div>
+</div>
+</BrowserRouter>
   </IonApp>
-);
+); 
 
 export default App;
